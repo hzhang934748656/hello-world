3fb5754ac22e2c0dec2f1704332fd1666536e8f7
hzhang934748656 <44934005+hzhang934748656@users.noreply.github.com>
2020-03-21 17:36:32 -0400

    Update Leetcode34.py

--- Leetcode/Leetcode34.py
+++ Leetcode/Leetcode34.py
@@ -1,11 +1,6 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         l, r = 0, len(nums) - 1
-        if len(nums) == 1:
-            if nums[0] == target:
-                return [0,0]
-            else:
-                return [-1,-1]
         while l <= r:
             mid =  (l+r) // 2
             if nums[mid] == target:
