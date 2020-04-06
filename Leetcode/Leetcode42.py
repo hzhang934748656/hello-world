6cb1aa682bb525cd0e946e99c4708f50f7edb911
hzhang934748656 <44934005+hzhang934748656@users.noreply.github.com>
2020-04-06 18:39:48 -0400

    Create Leetcode42.py

---
+++ Leetcode/Leetcode42.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        if n < 3:
+            return 0
+        left_max = [0] * n
+        left_max[0] = height[0]
+        right_max = [0] * n
+        right_max[n-1] = height[n-1]
+        for i in range(1,n):
+            left_max[i] = max(left_max[i-1],height[i])
+        for i in range(n-2,-1,-1):
+            right_max[i] = max(right_max[i+1],height[i])
+        output = 0
+        for i in range(n):
+            output += min(left_max[i],right_max[i]) - height[i]
+        return output
